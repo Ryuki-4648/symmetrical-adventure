86a4b2ad71dc69a2ce3f78d2015116ee0b8eab3e
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-08-26 23:23:25 +0900

    ハンバーガーメニューの開閉実装

--- invitation/src/components/Header.js
+++ invitation/src/components/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 function Header({headerMenu}) {
+  const handleClickHeaderButton = () => {
+    const headerContent = document.querySelector('.l-headerContent');
+    headerContent.classList.add('is-open');
+  }
+  const handleCloseHeaderContent = () => {
+    const headerContent = document.querySelector('.l-headerContent');
+    headerContent.classList.remove('is-open');
+  }
   return (
-    <header className={`l-header ${headerMenu ? 'is-active' : ''}`}>
-      <button className='l-header__button'>ME<br />NU</button>
-    </header>
+    <>
+      <header className={`l-header ${headerMenu ? 'is-active' : ''}`} onClick={handleClickHeaderButton}>
+        <button className='l-header__button'>ME<br />NU</button>
+      </header>
+
+      <div className='l-headerContent'>
+        <button className='l-headerContent__button' onClick={handleCloseHeaderContent}>
+          <span className='l-headerContent__buttonBorder'></span>
+          <span className='l-headerContent__buttonBorder'></span>
+          <p className='l-headerContent__buttonText'>Close</p>
+        </button>
+      </div>
+    </>
   )
 }
 
