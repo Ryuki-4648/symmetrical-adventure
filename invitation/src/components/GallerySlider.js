2fb1a8ffa801bcef589fc62e721e330e64a7ca85
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2024-08-28 02:22:42 +0900

    ギャラリーをSwiperを使わない方法に修正

--- invitation/src/components/GallerySlider.js
+++ invitation/src/components/GallerySlider.js
@@ -7,6 +7,8 @@ import 'swiper/css/effect-creative';
 
 // import required modules
 import { Autoplay } from 'swiper/modules';
+import { useState } from 'react';
+import { Modal } from '@chakra-ui/react';
 
 function GallerySlider() {
 
@@ -18,24 +20,57 @@ function GallerySlider() {
    */
   const imageFileName = Array.from({ length: 9 }, (_, i) => `/prod/gallery/img${String(i + 1).padStart(2, '0')}.jpg`);
 
+  /* クリックで画像モーダル表示 */
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState('');
+
+  const handleOpenModal = (targetImage) => {
+    setModalOpen(true);
+    setSelectedImage(targetImage);
+  };
+  const handleCloseModal = () => {
+    setModalOpen(false);
+    setSelectedImage('');
+  }
+
   return (
-    <Swiper
-    watchSlidesProgress={true}
-    slidesPerView={3}
-    // autoplay={{delay: 1000}}
-    loop={false}
-    modules={[Autoplay]}
-    // speed={3000}
-    navigation
-    pagination={{ clickable: true }}
-    className="mySwiper"
-  >
-    {imageFileName.map((src, index) => (
-      <SwiperSlide key={index}>
-        <img src={src} alt={`ギャラリー画像 ${index + 1}`} className='l-gallery__image' />
-      </SwiperSlide>
-    ))}
-  </Swiper>
+    // <Swiper
+    //   watchSlidesProgress={true}
+    //   slidesPerView={3}
+    //   // autoplay={{delay: 1000}}
+    //   loop={false}
+    //   modules={[Autoplay]}
+    //   // speed={3000}
+    //   navigation
+    //   pagination={{ clickable: true }}
+    //   className="mySwiper"
+    // >
+    //   {imageFileName.map((src, index) => (
+    //     <SwiperSlide key={index}>
+    //       <img src={src} alt={`ギャラリー画像 ${index + 1}`} className='l-gallery__image' />
+    //     </SwiperSlide>
+    //   ))}
+    // </Swiper>
+    <>
+      <ul className='c-gallery'>
+        {imageFileName.map((src, index) => (
+          <li className='c-gallery__item' onClick={handleOpenModal(src)}>
+            <img src={src} alt={`ギャラリー画像 ${index + 1}`} className='c-gallery__image' />
+          </li>
+        ))}
+      </ul>
+
+      <Modal
+        isOpen={modalOpen}
+        onClose={handleCloseModal}
+        contentLabel='Gallery Modal'
+        className='c-modal'
+        overlayClassName='c-modal__overlay'
+      >
+        <button onClick={handleCloseModal} className='c-modal__close'>閉じる</button>
+        <img src={selectedImage} alt='ギャラリー画像' className='c-modal__image' />
+      </Modal>
+    </>
   )
 }
 
